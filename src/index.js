6d540b5db22ddd431feb09db1ee58c6675eb165c
Dmytro Martynenko <MartynenkoDV@comfy.ua>
2022-07-11 21:11:24 +0300

    add devServe config to webpack.config

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import {Teacher} from './Teacher';
 import {Student} from './Student';
 
 const student = new Student('Victor', 20, 'male', 'Guitar, footbal, games...');
-const teacher = new Teacher('Sam', 55, 'male', 'Guns, cars, teach, etc.', student);
+const teacher = new Teacher('John', 55, 'male', 'Guns, cars, teach, etc.', student);
 
 console.log(teacher);
 console.log(student);
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,13 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'src'),
+        },
+        compress: true,
+        port: 9000,
+    },
     module: {
         rules: [
             { test: /\.(js)$/, use: 'babel-loader' }
